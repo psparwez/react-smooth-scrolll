95d83bf717102d6727b3137e19bb95fa3cace3c9
PS Parwez <infoparwez200@gmail.com>
2025-02-28 13:46:44 +0530

    fix: hydration error

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -4,9 +4,7 @@ export const Footer = () => {
     return (
         <footer className="py-8 px-0 border-t border-dashed border-zinc-400">
             <div className="container">
-                <p className="flex items-center gap-3 m-0 text-sm"
-
-                >
+                <div className="flex items-center gap-3 m-0 text-sm">
                     {/* IMAGE */}
                     <div className="size-6 rounded-full bg-gradient-to-r from-teal-400 to-yellow-200" />
 
@@ -14,7 +12,7 @@ export const Footer = () => {
                         Made by{' '}
                         <Link href="https://github.com/psparwez/" className="font-medium text-blue-500 after:content-['_↗']" target='_blank'>psparwez</Link>
                     </span>
-                </p>
+                </div>
             </div>
         </footer>
     );
